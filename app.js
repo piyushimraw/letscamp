e286e1c6af6ed6741db271fe10aadf83ba98e1a4
piyushimraw <piyush.srivastavaimraw@gmail.com>
2017-11-11 13:08:43 +0530

    added data persistance to the campground and add new campground is working

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-var express = require ("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express 	= require ("express");
+var app 		= express();
+var bodyParser  = require("body-parser");
+var mongoose 	= require ("mongoose");
+
+var mongoDB = 'mongodb://127.0.0.1/letscamp';
+
+mongoose.connect(mongoDB, { 
+	useMongoClient : true
+}); 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -8,13 +15,38 @@ app.set("view engine", "ejs");
 
 
 
-var campgrounds = [
-		{name: "Okhala", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-		{name: "River's Dawn", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-		{name: "Bird's Paradise", image: "https://farm3.staticflickr.com/2924/14465824873_026aa469d7.jpg"},
-		{name: "Adventure Rocks", image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg"}, 
-	]
+// var campgrounds = [
+// 		{name: "Okhala", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+// 		{name: "River's Dawn", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+// 		{name: "Bird's Paradise", image: "https://farm3.staticflickr.com/2924/14465824873_026aa469d7.jpg"},
+// 		{name: "Adventure Rocks", image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg"}, 
+// 	]
 
+//get the default connection
+var db  = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongoDB connection error'));
+
+//defining schema
+var Schema = mongoose.Schema;
+var campgroundsSchema  = new Schema ({
+	name : String,
+	image : String
+});
+
+
+//compile model from schema 
+var Campground = mongoose.model('campgrounds', campgroundsSchema);
+
+//inserting record into collection 
+// Campground.create({
+// 	name: 'Awesome Place',
+// 	image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'
+// }, function(err, campground) {
+// 	if(err) console.log(err);
+// 	else{
+// 		console.log(campground);
+// 	}
+// });
 
 
 app.get("/", function(req, res){
@@ -22,7 +54,14 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-	res.render("campgrounds", {campgrounds : campgrounds});
+	Campground.find({}, function(err, allcampgrounds){
+		if(err){
+			console.log(err);
+		}else {
+			res.render("campgrounds", {campgrounds : allcampgrounds});
+		}
+	});
+	
 });
 
 app.get("/campgrounds/new", function(req,res){
@@ -35,9 +74,17 @@ app.post("/campgrounds", function(req, res){
 	var image = req.body.image;
 
 	var newCampground = {name: name, image: image};
-	campgrounds.push(newCampground);
-	//redirect back  to cammpgrounds
-	res.redirect("/campgrounds");
+	//adding campground to database
+	Campground.create(newCampground, function(err, campground){
+		if(err){
+			console.log(err);
+		}
+		else{
+			//redirect back  to cammpgrounds
+			res.redirect("/campgrounds");
+		}
+	});
+
 });
 
 
