b8d662e1a5c97e64f84d76424100741c81d8ce15
piyushimraw <piyush.srivastavaimraw@gmail.com>
2018-01-03 21:09:27 +0530

    added db username and pass

--- models/db.js
+++ models/db.js
@@ -1,9 +1,9 @@
 var mongoose 	= require ("mongoose");
-const mongoDB =  'mongodb://localhost:27017/lets_camp' || mongodb://piyushshh:Cr33pyPa$ta$@ds125146.mlab.com:25146/lets_camp;
+const mongoDB =  "mongodb://localhost:27017/lets_camp" || "mongodb://piyushshh:Cr33pyPa$ta$@ds125146.mlab.com:25146/lets_camp";
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoDB, { 
-	useMongoClient : true
+    useMongoClient : true
 }); 
 
 
@@ -12,7 +12,7 @@ mongoose.connect(mongoDB, {
 
 //get the default connection
 var db  = mongoose.connection;
-db.on('error', console.error.bind(console, 'mongoDB connection error'));
+db.on("error", console.error.bind(console, "mongoDB connection error"));
 
 
 module.exports = db;
