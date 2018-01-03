f9dfffbdac10f3b7492ad727d36938e0fffc3c01
piyushimraw <piyush.srivastavaimraw@gmail.com>
2018-01-03 20:58:43 +0530

    Modified index

--- index.js
+++ index.js
@@ -1,78 +1,3 @@
-<<<<<<< HEAD
-var app 		= require('./server');
-var Campground 	= require ('./models/campground');
-var Comment 	= require ('./models/comment');
-
-app.get("/", function(req, res){
-	res.render("landing")
-});
-
-app.get("/campgrounds", function(req, res){
-	Campground.find({}, function(err, allcampgrounds){
-		if(err){
-			console.log(err);
-		}else {
-			res.render("index", {campgrounds : allcampgrounds});
-		}
-	});
-	
-});
-
-app.get("/campgrounds/new", function(req,res){
-	res.render("new");
-});
-
-app.post("/campgrounds", function(req, res){
-	//get data from form and add to campground array
-	var name = req.body.name;
-	var image = req.body.image;
-	var description = req.body.description;
-	var newCampground = { name: name, image: image, description : description };
-
-	//adding campground to database
-	Campground.create(newCampground, function(err, campground){
-		if(err){
-			//if error happens handle error
-			console.log(err);
-		}
-		else{
-			//redirect back  to cammpgrounds
-			res.redirect("/campgrounds");
-		}
-	});
-
-});
-
-app.get("/campgrounds/:id", function(req, res){
-	//caputure the id
-	id = req.params.id;
-	Campground.findById(id).populate('comments').exec (function(err, foundCampground){
-		if(err){
-			console.log(err);
-		}else{
-			res.render("show", { foundCampground : foundCampground });
-		}
-	})
-});
-
-app.post('/campgrounds/:id/comment', function(req, res){
-	Comment.create(req.body.data, function(err, comment){
-		if(err) console.log(err);
-		else{
-			Campground.findById(req.params.id, function(err, campground){
-				if (err) console.log(err);
-				else{
-					campground.comments.push(comment._id);
-					campground.save();
-					res.redirect('/campgrounds/'+req.params.id);
-				}
-
-			});
-		}
-	});
-});
-
-=======
 var app 	= require("./server"),
 	seedDB	= require("./seeds");
 
@@ -85,7 +10,6 @@ var campgroundRoutes = require("./routes/campgrounds"),
 app.use("/",campgroundRoutes);
 app.use("/",commentRoutes);
 app.use("/",authRoutes);
->>>>>>> dev
 
 const port = process.env.PORT || 8080;
 const ip = process.env.IP || "localhost";
